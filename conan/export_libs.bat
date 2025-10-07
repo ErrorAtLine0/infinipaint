c2cdc476a55cfc453fc612ab3484029fa303c866
Yousef Khadadeh <erroratline0@gmail.com>
2025-10-07 17:31:06 +0300

    Rollback libdatachannel to v0.23.1 due to issues with windows compilation

--- conan/export_libs.bat
+++ conan/export_libs.bat
@@ -1,3 +1,3 @@
 conan export %0/../libjuice/all/conanfile.py --version=1.6.1
-conan export %0/../libdatachannel/all/conanfile.py --version=0.23.2
+conan export %0/../libdatachannel/all/conanfile.py --version=0.23.1
 conan export %0/../conan-skia/recipes/skia/all/conanfile.py --version=134.20250327.0
