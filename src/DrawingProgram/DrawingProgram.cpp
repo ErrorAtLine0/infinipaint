0efcfe0b2797ea5a396592eb950747f35b4bcd41
Yousef Khadadeh <erroratline0@gmail.com>
2026-09-08 18:17:58 +0300

    Up version number to v0.6.1

--- src/DrawingProgram/DrawingProgram.cpp
+++ src/DrawingProgram/DrawingProgram.cpp
@@ -120,9 +120,11 @@ void DrawingProgram::input_text_callback(const InputManager::TextCallbackArgs& t
 }
 
 void DrawingProgram::input_mouse_button_callback(const InputManager::MouseButtonCallbackArgs& button) {
+    if(button.deviceType == InputManager::MouseDeviceType::TOUCH && world.main.conf.disableTouchForDrawing)
+        return;
+
     auto buttonCallbacks = [&](const InputManager::MouseButtonCallbackArgs& b) {
-        if(b.deviceType != InputManager::MouseDeviceType::TOUCH || !world.main.conf.disableTouchForDrawing)
-            drawTool->input_mouse_button_on_canvas_callback(b);
+        drawTool->input_mouse_button_on_canvas_callback(b);
     };
 
     if(button.down) {
@@ -174,8 +176,10 @@ void DrawingProgram::input_mouse_button_callback(const InputManager::MouseButton
 }
 
 void DrawingProgram::input_mouse_motion_callback(const InputManager::MouseMotionCallbackArgs& motion) {
-    if(motion.deviceType != InputManager::MouseDeviceType::TOUCH || !world.main.conf.disableTouchForDrawing)
-        drawTool->input_mouse_motion_callback(motion);
+    if(motion.deviceType == InputManager::MouseDeviceType::TOUCH && world.main.conf.disableTouchForDrawing)
+        return;
+
+    drawTool->input_mouse_motion_callback(motion);
 }
 
 void DrawingProgram::input_key_callback(const InputManager::KeyCallbackArgs& key) {
