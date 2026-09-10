bcadd5d68f94c38cfb29d5f27c2f65e9e02ba62f
Yousef Khadadeh <erroratline0@gmail.com>
2026-09-10 11:46:13 +0300

    Use flatpak flatpak-builder instead of distribution package

--- linuxinstall/flatpakbuilddir/create_flatpak.sh
+++ linuxinstall/flatpakbuilddir/create_flatpak.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo builddir ../com.infinipaint.infinipaint.yml
+flatpak run org.flatpak.Builder --force-clean --user --install-deps-from=flathub --repo=repo builddir ../com.infinipaint.infinipaint.yml
 flatpak build-bundle repo infinipaint.flatpak com.infinipaint.infinipaint
